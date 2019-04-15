9e3e8fd08cfc7a9e7361c6b612d4ca423e1952e3
Matheus Johann Araújo <47996428+matheusjohannaraujo@users.noreply.github.com>
2019-04-15 17:49:07 -0300

    Update piano_musical.ino

--- piano_musical/piano_musical.ino
+++ piano_musical/piano_musical.ino
@@ -9,6 +9,13 @@
 */
 
 // Notas
+#define NOTE_C4 262
+#define NOTE_D4 294
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_A4 440
+#define NOTE_B4 494
 #define NOTE_C5 523
 #define NOTE_D5 587
 #define NOTE_E5 659
@@ -16,14 +23,6 @@
 #define NOTE_G5 784
 #define NOTE_A5 880
 #define NOTE_B5 988
-
-#define NOTE_C4  262
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_G4  392
-#define NOTE_A4  440
-#define NOTE_B4  494
 
 // Pinos
 #define pinBuzz 10
@@ -39,7 +38,7 @@ const int pinLed[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 
 // Constantes
 #define delayBuzz 250
-#define maxDistan 10
+#define maxDistan 25
 
 void setup() {
   setPinOut(pinUltraTrig, sizeof(pinUltraTrig) / sizeof(int));
